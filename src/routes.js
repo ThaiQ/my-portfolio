bcbc40b7326dfc9e0879c2fe98ef0e1810f23a4b
ThaiQ <quachhongthai200@gmail.com>
2020-08-11 15:35:02 -0700

    Remove route logs and extra routes

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //import pages
 import Home from './pages/Home/home'
-import Contact from './pages/Contact/contact'
 
 export default function Routes(props) {
 
@@ -11,10 +10,6 @@ export default function Routes(props) {
         {
             path: '/',
             component: Home
-        },
-        {
-            path: '/contact',
-            component: Contact
         }
     ]
 
@@ -22,10 +17,10 @@ export default function Routes(props) {
         <Router>
             <Switch>
                 {routes.map((route, index) => {
-                    console.log(route)
                     return <Route exact key={index}
                         path={route.path} component={route.component} />
                 })}
+                <Route component={Home} />
             </Switch>
         </Router>
     )
