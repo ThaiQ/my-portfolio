253db6fd2202a830075ee7f8ca2405eb5e78ed9c
ThaiQ <quachhongthai200@gmail.com>
2020-08-19 22:46:15 -0700

    resize about page

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -1,36 +1,57 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactRotatingText from 'react-rotating-text'
 import './about.css'
 
-export default class About extends Component {
+// Hook
+function useWindowSize() {
+    // Initialize state with undefined width/height so server and client renders match
+    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+    return windowSize;
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(),
-        };
-    }
+export default function About () {
 
-    render() {
-        return (<div id='about'>
-            <div className='grid-container'>
-                <h1 className='title'>About me!</h1>
+    const size = useWindowSize();
 
-                <p className='sub-title text'> Software engineering student at San Jose State University. </p>
+    return (<div id='about'>
+        <div className='grid-container'>
+            <h1 className='title'>About me!</h1>
 
-                <p className='sub-title2 text'> Graduating on December-2021. </p>
+            <p className='sub-title text'> Software engineering student at San Jose State University. </p>
 
-                <p className='bio-text text'>
-                    My experiences revolve around developing
-                    full-stack application and quality assurance.
-                    I am well-versatile with web-development and web-frameworks such as React.
-                </p>
+            <p className='sub-title2 text'> Graduating on December-2021. </p>
 
-                <p className='fun-text text'> 
-                I am looking for opportunities to 
-                <ReactRotatingText items={[' discover new technologies!', ' learn new skills!', ' develop professionally!']} /> 
-                </p>
-            </div>
-        </div>)
-    }
+            <p className='bio-text text'>
+                My experiences revolve around developing
+                full-stack application and quality assurance.
+                I am well-versatile with web-development and web-frameworks such as React.
+            </p>
+
+            <p className='fun-text text'> 
+            {size.width > 450 ?  'I am excited to' 
+             : <div> I am excited to </div>}
+             <ReactRotatingText items={[' discover new technologies!', ' learn new skills!', ' develop professionally!']} />
+            </p>
+        </div>
+    </div>)
 }
