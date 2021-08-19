8a058d7b8a2df3be0034101819bc8c337537d87e
ThaiQ <quachhongthai200@gmail.com>
2021-08-19 10:39:24 -0700

    Updates

---
+++ precache-manifest.e6e3a2625dd34415952aa9a1317dc8b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3b752f85861d81995f96c3e8852225",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26c8ba021276a22f7e8a",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a05d436b7db272fa95aa",
+    "url": "/static/css/main.0a320d0d.chunk.css"
+  },
+  {
+    "revision": "26c8ba021276a22f7e8a",
+    "url": "/static/js/2.2273cad9.chunk.js"
+  },
+  {
+    "revision": "e4a7c0844670e16d96bf57f70219597d",
+    "url": "/static/js/2.2273cad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05d436b7db272fa95aa",
+    "url": "/static/js/main.0b99731c.chunk.js"
+  },
+  {
+    "revision": "18a1de75f68d75973a09",
+    "url": "/static/js/runtime-main.db4ada96.js"
+  }
+]);
